b2eee215565baca286e224d599fa4fe5cd9fb70e
yuju <yujuhyeon@ksmartech.com>
2020-09-01 13:03:42 +0900

    change to import annotation all

--- src/main/java/com/cal_holi/demo/MainController.java
+++ src/main/java/com/cal_holi/demo/MainController.java
@@ -4,9 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
@@ -16,7 +14,6 @@ public class MainController {
 
     @Autowired
     MainService mainService;
-
     @GetMapping(value = "/empInfo/{empNo}")
     @ResponseBody
     public GetEmpInfoDto empInfo(@PathVariable String empNo){
